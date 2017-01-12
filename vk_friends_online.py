84120a87bbe0c9980b1946433371eb58569a8b82
manchos <vladm.ru@gmail.com>
2017-01-12 14:09:19 +0300

    finish

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,15 +1,16 @@
 import vk
+from getpass import getpass
 
 
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+APP_ID = 5818816
 
 
 def get_user_login():
-    pass
+    return input('Enter the username:')
 
 
 def get_user_password():
-    pass
+    return getpass('Please enter your password: ')
 
 
 def get_online_friends(login, password):
@@ -17,16 +18,25 @@ def get_online_friends(login, password):
         app_id=APP_ID,
         user_login=login,
         user_password=password,
+        scope='friends'
     )
     api = vk.API(session)
-    # например, api.friends.get()
+    friends_online_ids = api.friends.getOnline(user_id="")
+    return api.users.get(user_ids=friends_online_ids)
 
 
 def output_friends_to_console(friends_online):
-    pass
+    print("Your friends online:")
+    for friend in friends_online:
+        print("id: {uid}\nfirst name: {first_name}\nlast name: {last_name}\n".format(**friend))
+
 
 if __name__ == '__main__':
     login = get_user_login()
     password = get_user_password()
-    friends_online = get_online_friends(login, password)
+    try:
+        friends_online = get_online_friends(login, password)
+    except vk.exceptions.VkAuthError:
+        print("The username and/or password are not correct.")
+        exit(1)
     output_friends_to_console(friends_online)
